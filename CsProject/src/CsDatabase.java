417aa41b9ee42ead2a0d7e1fdfc3ce456d5b8276
Rasmus <Rasmus@DESKTOP-IO15SRG>
2018-03-11 21:22:49 +0100

    don don!

--- CsProject/src/CsDatabase.java
+++ CsProject/src/CsDatabase.java
@@ -6,15 +6,13 @@ import java.util.Scanner;
 	Run with:
 	java -cp postgresql-42.2.1.jar:. DBTest
  */
-
 public class CsDatabase {
 
     String url = "jdbc:postgresql://horton.elephantsql.com:5432/ylsaiyqk";
     String username = "ylsaiyqk";
     String password = "G8hPbcpjJKe9gV8mUhAOAr-SM7YgsKgy";
 
-    
-    public void runloop(){
+    public void runloop() {
         boolean run = true;
         Scanner sc = new Scanner(System.in);
         char command;
@@ -24,60 +22,36 @@ public class CsDatabase {
                 + "\n c:\t A List of all names of teams and the number of players on that team."
                 + "\n d:\t A list of turnaments whit a minimum number of partisipating teams based on a input number."
                 + "\n pleas enter the char of the disired query (a,b,c,d,) and then hit the retrun key, input q if you want to exit the application.");
-        while(run){
+        while (run) {
             command = sc.next().charAt(0);
-            if (command == 'a'){
-               assignmentA();
-            }
-            else if (command == 'b'){
+            if (command == 'a') {
+                assignmentA();
+            } else if (command == 'b') {
                 assignmentB();
-            }
-            else if (command == 'c'){
+            } else if (command == 'c') {
                 assignmentC();
-            }
-            else if (command == 'd'){
+            } else if (command == 'd') {
                 System.out.println("Please input the number of Partisipating teams");
                 assignmentD(sc.nextInt());
-            }
-            else if (command == 'q'){
+            } else if (command == 'q') {
                 run = false;
                 System.out.println("you have quit the application");
+            } else {
+                System.out.println("your input didnt register correct pleas try again");
             }
-            else{
-                System.out.println("your input didnt register correct pleas try again"); 
-            }
-                    }
+        }
     }
-    public void queryTest() {
+    private void assignmentA() {
+        // List of all names of coaches and the team they belong to.
         try {
             Connection db = DriverManager.getConnection(url, username, password);
 
             Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("select * from Player");
-            while (rs.next()) {
-
-                System.out.print(rs.getString(1) + " ");
-                System.out.println(rs.getString(2) + " ");
-            }
-            rs.close();
-            st.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    private void assignmentA() {
-        // List of all names of coaches and the team they belong to.
-          try {
-            Connection db = DriverManager.getConnection(url, username, password);
-
-            Statement st = db.createStatement();
             ResultSet rs = st.executeQuery("select name, teamname from people, coaches where people.email = coaches.email");
+            System.out.printf("|%1$-35s|%2$-35s|%n", rs.getMetaData().getColumnName(1), rs.getMetaData().getColumnName(2));
             while (rs.next()) {
 
-                System.out.print("Coach name: " + rs.getString(1) + " ");
-                System.out.println("Team name: " + rs.getString(2) + " ");
+                System.out.printf("|%1$-35s|%2$-35s|%n", rs.getString(1), rs.getString(2));
             }
             rs.close();
             st.close();
@@ -88,15 +62,15 @@ public class CsDatabase {
     }
 
     private void assignmentB() {
-          try {
+        try {
             Connection db = DriverManager.getConnection(url, username, password);
 
             Statement st = db.createStatement();
             ResultSet rs = st.executeQuery("select name, b.teamname from people,(select email, player.teamname from player inner join winner on player.teamname = winner.teamname union Select email, coaches.teamname from coaches inner join Winner on coaches.Teamname = Winner.Teamname) AS b where b.email = people.email;");
+            System.out.printf("|%1$-35s|%2$-35s|%n", rs.getMetaData().getColumnName(1), rs.getMetaData().getColumnName(2));
             while (rs.next()) {
 
-                System.out.print(rs.getString(1) + " ");
-                System.out.println(rs.getString(2) + " ");
+                System.out.printf("|%1$-35s|%2$-35s|%n", rs.getString(1), rs.getString(2));
             }
             rs.close();
             st.close();
@@ -104,7 +78,7 @@ public class CsDatabase {
         } catch (Exception e) {
             System.out.println(e);
         }
-    
+
     }
 
     private void assignmentC() {
@@ -113,10 +87,10 @@ public class CsDatabase {
 
             Statement st = db.createStatement();
             ResultSet rs = st.executeQuery("select teamname, COUNT(*) from Player GROUP BY Teamname");
+            System.out.printf("|%1$-35s|%2$-35s|%n", rs.getMetaData().getColumnName(1), rs.getMetaData().getColumnName(2));
             while (rs.next()) {
 
-                System.out.print(rs.getString(1) + " ");
-                System.out.println(rs.getString(2) + " ");
+                System.out.printf("|%1$-35s|%2$-35s|%n", rs.getString(1), rs.getString(2));
             }
             rs.close();
             st.close();
@@ -124,11 +98,11 @@ public class CsDatabase {
         } catch (Exception e) {
             System.out.println(e);
         }
-    
+
     }
 
     private void assignmentD(int nextInt) {
-try {
+        try {
             Connection db = DriverManager.getConnection(url, username, password);
 
             Statement st = db.createStatement();
@@ -147,8 +121,8 @@ try {
             System.out.println(e);
         }
     }
-    
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (java.lang.ClassNotFoundException e) {
@@ -156,7 +130,6 @@ try {
         }
         CsDatabase test = new CsDatabase();
 
-//        test.queryTest();
         test.runloop();
 
     }
